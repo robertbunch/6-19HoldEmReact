3f023422e734cebaf08b06b84199f610d1b206b4
Robert Bunch <rdbunch@gmail.com>
2019-09-10 10:43:45 -0400

    Add bet and msg

--- src/components/PokerTable.js
+++ src/components/PokerTable.js
@@ -6,14 +6,16 @@ import './PokerTable.css';
 class PokerTable extends Component{
     constructor(){
         super();
-        this.deck = new Deck();
-        this.deck.create();
-        this.deck.shuffle();
+        this.deck = new Deck(); //make a new Deck object 
+        this.deck.create(); // create a deck from the Deck object
+        this.deck.shuffle(); // shuffle the deck
         console.log(this.deck.cards);
         this.state = {
-            playerHand: [],
-            dealerHand: [],
-            communityHand: []
+            playerHand: ['deck','deck'], //this is the player array of cards
+            dealerHand: ['deck','deck'],
+            communityHand: ['deck','deck','deck','deck','deck'],
+            wager: 0,
+            bankroll: 100
         }
     }
 
@@ -35,13 +37,56 @@ class PokerTable extends Component{
         })
     };
 
+    bet = (amount)=>{
+        // NO NO NO NO NO NO NO NO NO
+        // this.state.wager += amount;
+        const newWager = this.state.wager + amount;
+        const newBankRoll = this.state.bankroll - amount;
+        if(newBankRoll >= 0){
+            this.setState({
+                wager: newWager,
+                bankroll: newBankRoll
+            })
+        }else{
+            this.setState({
+                msg: "You don't have enough money!"
+            },this.clearMsg)
+        }
+    }
+
+    clearMsg = ()=>{
+        setTimeout(()=>{
+            this.setState({
+                msg:""
+            })
+        },2000)
+    }
+
+
     render(){
         return(
             <div className="the-table col-sm-12">
+                <div className="col-sm-12 text-center the-numbers">
+                    <div className="col-sm-3 col-sm-offset-3">
+                        Current Pot: ${this.state.wager}
+                    </div>
+                    <div className="col-sm-3">
+                        Bankroll: ${this.state.bankroll}
+                    </div>
+                </div>
+                <div className="player-message">
+                    {this.state.msg}                    
+                </div>
+
                 <PokerHand cards={this.state.playerHand} />   {/* Player 1 hand */}
                 <PokerHand cards={this.state.communityHand} />   {/* Community hand */}
                 <PokerHand cards={this.state.dealerHand} />   {/* Player 2 hand */}
-                <button onClick={this.prepDeck} className="btn btn-primart">Start</button>
+                <div className="col-sm-12 buttons">
+                    <button onClick={this.prepDeck} className="btn btn-primary">Deal</button>
+                    <button onClick={()=>{this.bet(5)}} className="btn btn-success">Bet 5</button>
+                    <button onClick={this.prepDeck} className="btn btn-warning">Check</button>
+                    <button onClick={this.prepDeck} className="btn btn-danger">Fold</button>
+                </div>
             </div>
         )
     }
